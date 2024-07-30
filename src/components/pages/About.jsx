dc23ea913a5de1da1f463397ff00c4110ebafbe6
anshumancodes <anshumanprof01@gmail.com>
2024-07-30 15:40:15 +0530

    add pages about and counsil"

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>About
+       <p>this page is still under development</p>
+    </div>
+   
+  )
+}
+
---
+++ src/components/pages/Members.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Members = () => {
+  return (
+    <div>
+      <h1>Counsil</h1>
+      <p>this page is still under development</p>
+    </div>
+  )
+}
+
+export default Members
