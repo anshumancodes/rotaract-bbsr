6eb87d6acbc04a0657bbb96d4632ca27e42c64c9
anshumancodes <anshumanprof01@gmail.com>
2024-07-30 15:48:35 +0530

    add Gallery page

---
+++ src/components/pages/Gallery/Gallerypage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Gallery from '../Gallery'
+
+export const Gallerypage = () => {
+  return (
+    <div>
+       <h1>Gallery Page</h1>
+       <Gallery folderName={"work-drives"} />
+
+    </div>
+  )
+}
