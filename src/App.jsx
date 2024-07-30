5a0a431afda5362c4d10b304599e973f8a34c125
anshumancodes <anshumanprof01@gmail.com>
2024-07-30 15:35:53 +0530

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,32 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
+import { Home } from './components/pages/Home';
+import './index.css'
+import Blog from './components/pages/blog/Blog';
+import { Gallerypage } from './components/pages/Gallery/Gallerypage';
+import {About} from "./components/pages/About"
+import Members from './components/pages/Members';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home/>,
+  },
+  {
+    path: "/blog",
+    element: <Blog/>,
+  },
+  {
+    path: "/gallery",
+    element: <Gallerypage/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+  {
+    path: "/council",
+    element: <Members/>,
   },
 ]);
 function App() {
