ce895578985947a1828234bc6f237f721cb78fd6
anshumancodes <anshumanprof01@gmail.com>
2024-08-09 01:49:37 +0530

    add navbar , links and headings

--- src/components/pages/Gallery/Gallerypage.jsx
+++ src/components/pages/Gallery/Gallerypage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Gallery from '../Gallery'
+import Nav from '../../global/Nav'
+import Footer from '../../global/Footer'
 
 export const Gallerypage = () => {
   return (
-    <div>
-       <h1>Gallery Page</h1>
-       <Gallery folderName={"work-drives"} />
+    <div >
+      <Nav/>
+      <div className='w-full flex flex-col items-center'>
+      <h1 className='text-xl md:text-3xl font-bold mt-10 py-2'>Gallery Page</h1>
+      <Gallery folderName={"work-drives"} />
+      </div>
+      <Footer/>
 
     </div>
   )
