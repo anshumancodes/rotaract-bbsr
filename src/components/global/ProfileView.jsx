a81d2a45184cd472182fd29d4a55b5513d498df9
anshumancodes <anshumanprof01@gmail.com>
2024-08-09 01:42:48 +0530

    modified padding

--- src/components/global/ProfileView.jsx
+++ src/components/global/ProfileView.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
-export default function ProfileView({url,Name,link}) {
+export default function ProfileView({url,Name,link,post,show}) {
   return (
     <div>
-      <div className="flex-col flex gap-2 items-center justify-center py-5">
+      <div className="flex-col flex gap-2 items-center justify-center">
         <div className="profile__img">
           {url?  <img src={url} alt={`${Name}`} className='max-w-[200px]'/>:<img/>}
         </div>
         <div className="profile__name">
             <p className='font-bold text-xl'>{Name}</p>
         </div>
+        <div className="profile__name">
+            <p className='font-light text-gray-800'>{post}</p>
+        </div>
         <div className="profile__link">
-           {url?  <a href={`${link}`}><button className='bg-pink-primary px-2 py-1 text-base text-center text-white-primary'>read more</button></a>: <a></a>}
+           {url?  <a href={`${link}`}>{show ?<button className='bg-pink-primary px-2 py-1 text-base text-center text-white-primary'>read more</button>:null}</a>: <a></a>}
         </div>
       </div>
     </div>
