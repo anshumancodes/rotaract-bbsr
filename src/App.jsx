b88e06bdf42aaa2e10985cb3099bd3ed835c2203
anshumancodes <anshumanprof01@gmail.com>
2024-08-29 22:36:28 +0530

    add dashboard admin paths and auth context

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,67 @@
-import { useState } from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { Home } from './components/pages/Home';
-import './index.css'
-import Blog from './components/pages/blog/Blog';
-import { Gallerypage } from './components/pages/Gallery/Gallerypage';
-import {About} from "./components/pages/About"
-import Members from './components/pages/Members';
+import React from "react";
+import { Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./context/Authcontext"; // Adjust the path as necessary
+import ProtectedRoute from "./ProtectedRoute"; // Adjust the path as necessary
+import './index.css';
+
+// Lazy load components
+const Home = React.lazy(() => import('./components/pages/Home'));
+const Blog = React.lazy(() => import('./components/pages/blog/Blog'));
+const Gallerypage = React.lazy(() => import('./components/pages/Gallery/Gallerypage'));
+const About = React.lazy(() => import('./components/pages/About'));
+const Members = React.lazy(() => import('./components/pages/Members'));
+const Dashboard = React.lazy(() => import('./components/pages/dashboard/Dashboard'));
+const Admin = React.lazy(() => import("./components/pages/dashboard/AdminLogin"));
+const ForgotPass = React.lazy(() => import("./components/pages/dashboard/Forgotpassword"));
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Home />,
   },
   {
     path: "/blog",
-    element: <Blog/>,
+    element: <Blog />,
   },
   {
     path: "/gallery",
-    element: <Gallerypage/>,
+    element: <Gallerypage />,
   },
   {
     path: "/about",
-    element: <About/>,
+    element: <About />,
   },
   {
     path: "/council",
-    element: <Members/>,
+    element: <Members />,
   },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/forgot-password",
+    element:<ForgotPass/>
+  }
 ]);
-function App() {
-  
 
+function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+    <AuthProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
+     </AuthProvider>
+  );
 }
 
-export default App
+export default App;
