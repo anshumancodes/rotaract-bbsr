a2729c28a9fbd58f4f93adab0227eff1f83de9a1
anshumancodes <anshumanprof01@gmail.com>
2024-08-29 22:38:32 +0530

    create forgot password page

---
+++ src/components/pages/dashboard/Forgotpassword.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Forgotpassword = () => {
+  return (
+    <div>
+      <div class="m-10 flex max-w-lg flex-col border-8 border-gray-50 py-20 px-12 shadow-md">
+        <p class="font-serif text-4xl font-bold">To reset your credentials</p>
+        <p class="mt-3 font-medium">Send a mail to </p>
+        
+       
+        <button class="mt-10 bg-gray-900 px-6 py-3 text-white"><a href="mailto:anshumanprof01@gmail.com">anshumanprof01@gmail.com</a></button>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Forgotpassword;
