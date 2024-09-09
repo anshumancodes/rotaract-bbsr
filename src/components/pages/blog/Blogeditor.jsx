c7f9e2656d06940718282265a3015e6ddd81eb08
anshumancodes <anshumanprof01@gmail.com>
2024-09-09 21:31:27 +0530

    add Blog editor component to dashboard'

---
+++ src/components/pages/blog/Blogeditor.jsx
@@ -0,0 +1,351 @@
+import { useState, useEffect, useRef } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+
+import { db } from "../../../config/firebase";
+import { setDoc, doc, collection ,Timestamp } from "firebase/firestore";
+import "ckeditor5/ckeditor5.css";
+
+import {
+  ClassicEditor,
+  AccessibilityHelp,
+  Autoformat,
+  AutoImage,
+  AutoLink,
+  Autosave,
+  BalloonToolbar,
+  BlockQuote,
+  Bold,
+  Code,
+  CodeBlock,
+  Essentials,
+  FindAndReplace,
+  FullPage,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  HtmlComment,
+  HtmlEmbed,
+  ImageBlock,
+  ImageCaption,
+  ImageInline,
+  ImageInsert,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageStyle,
+  ImageTextAlternative,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  ListProperties,
+  Mention,
+  Paragraph,
+  SelectAll,
+  ShowBlocks,
+  SimpleUploadAdapter,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Strikethrough,
+  Table,
+  TableCellProperties,
+  TableProperties,
+  TableToolbar,
+  TextPartLanguage,
+  TextTransformation,
+  Title,
+  TodoList,
+  Underline,
+  Undo,
+} from "ckeditor5";
+export default function Blogeditor() {
+  const editorContainerRef = useRef(null);
+  const [slug, setSlug] = useState('');
+  const [editorData, setEditorData] = useState('');
+
+  const handleSave = async () => {
+    try {
+      // Create a reference to the document with the specified ID
+      const blogRef = doc(collection(db, "blog"), slug);
+
+      // Set the document data without creating an additional id field
+      await setDoc(blogRef, {
+        content:editorData,
+        createdAt: Timestamp.fromDate(new Date())
+      });
+
+      alert("article added  successfully!");
+     
+    } catch (error) {
+      console.error("Error creating blog: ", error);
+      alert("Error creating blog check console for errors");
+    }
+  };
+
+  const clearContent = () => {
+    setEditorData("<p></p>"); // Clears the content by setting it to an empty paragraph
+  };
+
+  const editorConfig = {
+    toolbar: {
+      items: [
+        "undo",
+        "redo",
+        "|",
+        "sourceEditing",
+        "showBlocks",
+        "|",
+        "heading",
+        "|",
+        "bold",
+        "italic",
+        "underline",
+        "|",
+        "link",
+        "insertImage",
+        "insertTable",
+        "highlight",
+        "blockQuote",
+        "codeBlock",
+        "|",
+        "bulletedList",
+        "numberedList",
+        "todoList",
+        "outdent",
+        "indent",
+      ],
+      shouldNotGroupWhenFull: false,
+    },
+    plugins: [
+      AccessibilityHelp,
+      Autoformat,
+      AutoImage,
+      AutoLink,
+      Autosave,
+      BalloonToolbar,
+      BlockQuote,
+      Bold,
+      Code,
+      CodeBlock,
+      Essentials,
+      FindAndReplace,
+      FullPage,
+      GeneralHtmlSupport,
+      Heading,
+      Highlight,
+      HorizontalLine,
+      HtmlComment,
+      HtmlEmbed,
+      ImageBlock,
+      ImageCaption,
+      ImageInline,
+      ImageInsert,
+      ImageInsertViaUrl,
+      ImageResize,
+      ImageStyle,
+      ImageTextAlternative,
+      ImageToolbar,
+      ImageUpload,
+      Indent,
+      IndentBlock,
+      Italic,
+      Link,
+      LinkImage,
+      List,
+      ListProperties,
+      Mention,
+      Paragraph,
+      SelectAll,
+      ShowBlocks,
+      SimpleUploadAdapter,
+      SourceEditing,
+      SpecialCharacters,
+      SpecialCharactersArrows,
+      SpecialCharactersCurrency,
+      SpecialCharactersEssentials,
+      SpecialCharactersLatin,
+      SpecialCharactersMathematical,
+      SpecialCharactersText,
+      Strikethrough,
+      Table,
+      TableCellProperties,
+      TableProperties,
+      TableToolbar,
+      TextPartLanguage,
+      TextTransformation,
+      Title,
+      TodoList,
+      Underline,
+      Undo,
+    ],
+    balloonToolbar: [
+      "bold",
+      "italic",
+      "|",
+      "link",
+      "insertImage",
+      "|",
+      "bulletedList",
+      "numberedList",
+    ],
+    heading: {
+      options: [
+        {
+          model: "paragraph",
+          title: "Paragraph",
+          class: "ck-heading_paragraph",
+        },
+        {
+          model: "heading1",
+          view: "h1",
+          title: "Heading 1",
+          class: "ck-heading_heading1",
+        },
+        {
+          model: "heading2",
+          view: "h2",
+          title: "Heading 2",
+          class: "ck-heading_heading2",
+        },
+        {
+          model: "heading3",
+          view: "h3",
+          title: "Heading 3",
+          class: "ck-heading_heading3",
+        },
+        {
+          model: "heading4",
+          view: "h4",
+          title: "Heading 4",
+          class: "ck-heading_heading4",
+        },
+        {
+          model: "heading5",
+          view: "h5",
+          title: "Heading 5",
+          class: "ck-heading_heading5",
+        },
+        {
+          model: "heading6",
+          view: "h6",
+          title: "Heading 6",
+          class: "ck-heading_heading6",
+        },
+      ],
+    },
+    htmlSupport: {
+      allow: [
+        {
+          name: /^.*$/,
+          styles: true,
+          attributes: true,
+          classes: true,
+        },
+      ],
+    },
+    image: {
+      toolbar: [
+        "toggleImageCaption",
+        "imageTextAlternative",
+        "|",
+        "imageStyle:inline",
+        "imageStyle:wrapText",
+        "imageStyle:breakText",
+        "|",
+        "resizeImage",
+      ],
+    },
+    initialData: "start writing",
+    link: {
+      addTargetToExternalLinks: true,
+      defaultProtocol: "https://",
+      decorators: {
+        toggleDownloadable: {
+          mode: "manual",
+          label: "Downloadable",
+          attributes: {
+            download: "file",
+          },
+        },
+      },
+    },
+    list: {
+      properties: {
+        styles: true,
+        startIndex: true,
+        reversed: true,
+      },
+    },
+    mention: {
+      feeds: [
+        {
+          marker: "@",
+          feed: [
+            /* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
+          ],
+        },
+      ],
+    },
+    menuBar: {
+      isVisible: true,
+    },
+    placeholder: "Type or paste your content here!",
+    table: {
+      contentToolbar: [
+        "tableColumn",
+        "tableRow",
+        "mergeTableCells",
+        "tableProperties",
+        "tableCellProperties",
+      ],
+    },
+  };
+  return (
+    <div>
+      <div className="main-container w-fit mx-auto">
+        <div
+          className="editor-container editor-container_classic-editor min-w-[795px] max-w-[795px]"
+          ref={editorContainerRef}
+        >
+          <div className="editor-container__editor">
+            <CKEditor
+              editor={ClassicEditor}
+              config={editorConfig}
+              data={editorData} // Bind editor content with state
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                setEditorData(data); // Update state with content changes
+              }}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 mt-4">
+          <button className="px-4 py-2 bg-green-400" onClick={handleSave}>
+            Save Blog
+          </button>
+          <button className="px-4 py-2 bg-red-600 ml-2" onClick={clearContent}>
+            Clear Content!
+          </button>
+          <input
+            type="text"
+            placeholder="Enter slug"
+            className="px-4 py-2 outline-none"
+            value={slug}
+            onChange={(e) => setSlug(e.target.value)} // Update slug state on change
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
